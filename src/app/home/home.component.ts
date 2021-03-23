667eaf5d585b7426441c5cae97cbfb03a36708f4
joas japhes <joasjaphes665@gmail.com>
2021-03-23 06:08:40 +0300

    added page animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routeAnimations } from '../shared/animations/router-animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations:[routeAnimations]
 })
 export class HomeComponent implements OnInit {
   isOpen = false;
