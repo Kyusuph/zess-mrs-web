20e46f5ba68d03720c3c0b50010b316ca7bfee3c
joas japhes <joasjaphes665@gmail.com>
2021-03-26 20:13:58 +0300

    added components

---
+++ src/app/modules/case/case-management/add-edit-case/add-edit-case.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-case',
+  templateUrl: './add-edit-case.component.html',
+  styleUrls: ['./add-edit-case.component.scss']
+})
+export class AddEditCaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
