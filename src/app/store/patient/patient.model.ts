b6c817ebd5018642cc5c4202999feb55bc14599b
Kayson Yusuph <yusuphkayxn@gmail.com>
2021-03-26 19:30:24 +0300

    Modifying partient model

--- src/app/store/patient/patient.model.ts
+++ src/app/store/patient/patient.model.ts
@@ -1,10 +1,12 @@
 export interface Patient {
     id:string;
     firstName:string;
+    middleName:string;
+    lastName:string;
     gender:string;
     phoneNumber:string;
     address:string;
     createdAt:string;
     updatedAt:string;
-    dateOfBirth:string;
+    dob:string;
 }
